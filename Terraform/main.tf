8749ef534066bacc03ac189cd899e6bb6186c4b4
letmeloveyou82 <shineanl04@gmail.com>
2025-05-20 09:46:47 +0900

    ✨feat: Terraform 보안 그룹 추가

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,18 @@
 module "network" {
-  source  = "./modules/network"
-  vpc_cidr = var.vpc_cidr
-  azs      = var.azs
+  source = "./modules/network"
+
+  vpc_cidr           = var.vpc_cidr
+  azs                = var.azs
+  ssh_port           = var.ssh_port
+  redis_port         = var.redis_port
+  https_port         = var.https_port
+  private_cidr_block = var.private_cidr_block
+}
+
+output "public_sg" {
+  value = module.network.public_sg_id
+}
+
+output "private_sg" {
+  value = module.network.private_sg_id
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,3 +7,23 @@ variable "azs" {
   type        = list(string)
   description = "List of Availability Zones"
 }
+
+variable "ssh_port" {
+  type        = number
+  description = "Port to allow SSH access"
+}
+
+variable "redis_port" {
+  type        = number
+  description = "Port to allow Redis access"
+}
+
+variable "https_port" {
+  type        = number
+  description = "Port to allow HTTPS access"
+}
+
+variable "private_cidr_block" {
+  type        = string
+  description = "CIDR block for private subnet internal traffic"
+}
